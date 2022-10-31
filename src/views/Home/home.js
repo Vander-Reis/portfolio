4027355a91234ab62a7d20d0e7264dcd0fe2d06f
Vander-Reis <vanderreis2017@outlook.com>
2022-10-31 11:16:50 -0300

    Efeito de digitação no titulo

--- src/views/Home/home.js
+++ src/views/Home/home.js
@@ -16,5 +16,22 @@ export default {
       
       mounted () {
         document.querySelector('body').style.background = "#020202"
+
+        const titulo = document.querySelector('p');
+
+        function typeWrite(elemento) {
+          const textArray = elemento.innerHTML.split('');
+          console.log(textArray)
+          elemento.innerHTML = ' ';
+
+          textArray.forEach((letra, i) => {
+            setTimeout(() => {
+              elemento.innerHTML += letra;
+            }, 75 * i);
+          });
+        }
+
+        typeWrite(titulo);
       },
+      
 }
